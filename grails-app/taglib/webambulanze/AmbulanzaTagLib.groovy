d6ea80b7f94a71624fcefec5adaa42ccd952512f
GuidoCeresa <guidoceresa@me.com>
2013-05-13 06:50:41 +0200

    fuield ultimo fix

--- grails-app/taglib/webambulanze/AmbulanzaTagLib.groovy
+++ grails-app/taglib/webambulanze/AmbulanzaTagLib.groovy
@@ -1447,7 +1447,7 @@ class AmbulanzaTagLib {
         String testoOut
         String testo
 
-        testo = 'Algos© - v2.6 del 12 maggio 2013'
+        testo = 'Algos© - v2.6 del 13 maggio 2013'
         testo = Lib.tagCella(testo, Aspetto.copyright)
         testoOut = Lib.tagTable(testo)
         return testoOut
